cc202e1b6ee191a2dc5c4a459fcb256c24482139
Trung <trung22112002@gmail.com>
2023-01-20 02:25:53 +0700

    nothing

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('<h1> Hello World! <h1>'))
+app.get('/', (req, res) => res.send('<h1> Test message <h1>'))
 
 app.listen(port, () => console.log('Listening on port ' + port))
