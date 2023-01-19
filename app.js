4744810d3e86763b954ccef4904e86ae2bc1e796
Trung <trung22112002@gmail.com>
2023-01-19 23:52:28 +0700

    modify notify message

--- app.js
+++ app.js
@@ -4,5 +4,4 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('<h1> Hello World! <h1>'))
 
-app.listen(port)
-console.log('Listening on port ' + port)
+app.listen(port, () => console.log('Listening on port ' + port))
